70e9e11c05507a78a034b19163d839a19a70703d
Kelvin Takyi-Bobi <52892093+KELs7@users.noreply.github.com>
2020-12-10 01:34:56 +0000

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,11 +1,21 @@
-function calcCy() {
+function calcCy1(e) {
+    calc_();
+    validate1(e);
+}   
+
+function calcCy2(e) {
+    calc_();
+    validate2(e);
+}   
+
+function calc_() {   
     var g = document.getElementById("ghRate").value;
     var e = document.getElementById("ethRate").value; 
     var u = document.getElementById("entry").value;
     var cc =document.getElementById("display");
     var select1 = document.getElementById("select1");
     var select2 = document.getElementById("select2");
-    
+
     if (select1.value =="eth" && select2.value=="ghc") {
         var result1 = u * e * g;
         cc.value = result1.toFixed(2);
@@ -21,17 +31,26 @@ function calcCy() {
     } else if (select1.value =="usd" && select2.value=="eth") {
         var result5 = u / e;
         cc.value = result5;
-    } else if (select1.value =="usd" && select2.value=="ghc") {
+    } else {
+        if (select1.value =="usd" && select2.value=="ghc") {
         var result6 = u * g;
         cc.value = result6.toFixed(2);
-    } else {
-        if (select1.value =="eth" && select2.value=="eth" || select1.value =="ghc" && select2.value=="ghc" || select1.value =="usd" && select2.value=="usd") {
-            select1.value = "eth";
-            select2.value = "ghc";
-            var result7 = u * e * g;
-            cc.value = result7.toFixed(2);
-        }
+        } 
     
     }
     
 }
+
+function validate1(e) {
+            e.value = e.value.replace(/\.{2,}/,"");
+            e.value = e.value.replace(/[\.][0-9]+[\.]/, "");
+            e.value = e.value.replace(/[-, ]/g,"");
+}
+
+function validate2(e) {
+            e.value = e.value.replace(/\.{2,}/,"");
+            e.value = e.value.replace(/[\.][0-9]{3}/, "");
+            e.value = e.value.replace(/[\.][0-9]{2}[\.]/, "");
+            e.value = e.value.replace(/[-, ]/g,"");
+}
+
